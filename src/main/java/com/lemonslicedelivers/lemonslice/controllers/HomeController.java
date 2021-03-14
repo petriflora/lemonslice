534f99e95243c74c475a6a42621906fdd42b5d3f
Jennifer Morgan <jemorgaine@gmail.com>
2021-03-13 19:13:08 -0600

    base webapp project with Angular loads and serves on localhost

--- src/main/java/com/lemonslicedelivers/lemonslice/controllers/HomeController.java
+++ src/main/java/com/lemonslicedelivers/lemonslice/controllers/HomeController.java
@@ -3,7 +3,6 @@ package com.lemonslicedelivers.lemonslice.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
